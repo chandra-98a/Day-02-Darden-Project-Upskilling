a80943824c17941cfa468acabeb7e5b8c9439832
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-21 15:25:52 +0530

    "created and run the testcase with @Test"

---
+++ Day-02-junit/src/main/java/org/capgemini/junit5/MathUtils.java
@@ -0,0 +1,7 @@
+package org.capgemini.junit5;
+
+public class MathUtils {
+public int add(int a, int b) {
+	return a+b;
+}
+}
---
+++ Day-02-junit/src/test/java/org/capgemini/junit5/MathUtilsTest.java
@@ -0,0 +1,15 @@
+package org.capgemini.junit5;
+
+
+
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+ void test() {
+	System.out.println("Testing is working");
+	}
+
+}
