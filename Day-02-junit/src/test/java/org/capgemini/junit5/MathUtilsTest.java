9dc35fb159b0ff70b143e933bf10601d031ec7e1
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-21 16:06:00 +0530

    "@TestInstance with no static for @BeforeAll"

--- Day-02-junit/src/test/java/org/capgemini/junit5/MathUtilsTest.java
+++ Day-02-junit/src/test/java/org/capgemini/junit5/MathUtilsTest.java
@@ -4,6 +4,7 @@ package org.capgemini.junit5;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,10 +13,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 	MathUtils mathUtils;
 	@BeforeAll
-	static void beforeAllInit() {
+ void beforeAllInit() {
 		System.out.println("This needs to run before all");
 	}
 
