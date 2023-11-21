d5a7eb5bcf550260049120d1f457f5225f986e9f
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-21 15:57:21 +0530

    "Using @BeforeEach"

--- Day-02-junit/src/test/java/org/capgemini/junit5/MathUtilsTest.java
+++ Day-02-junit/src/test/java/org/capgemini/junit5/MathUtilsTest.java
@@ -8,27 +8,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class MathUtilsTest {
+	MathUtils mathUtils;
+
+	@BeforeEach
+	void init() {
+		 mathUtils = new MathUtils();
+	}
 
 	    @Test
         void testAdd() {
-		MathUtils mathUtils= new MathUtils();
+		
 		int expected=5;
 		
-		int actual=mathUtils.add(2, 3);
+		int actual= mathUtils.add(2, 3);
 		assertEquals(expected,actual,"The add method should add two values");
 		
 	  }
 	    @Test
 		void testDivide() {
-	    	MathUtils mathUtils= new MathUtils();
+	    	
 			assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), 
 					"Divide should throw ArithmeticException when denominator is zero");
 		}
 		
 		@Test 
 		void testComputeCircleArea() {
-			MathUtils mathUtils= new MathUtils();
+			
 			assertEquals(314.1592653589793, 
 					mathUtils.computeCircleArea(10), 
 					"Should return right circle area");
