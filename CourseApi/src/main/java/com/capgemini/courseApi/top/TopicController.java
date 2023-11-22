629ce7a6edf7d66a7d227668d8cfe8a7f286ad5c
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-22 12:43:54 +0530

    "Using @RequestMapping and @Pathvariable to get the topic by id"

--- CourseApi/src/main/java/com/capgemini/courseApi/top/TopicController.java
+++ CourseApi/src/main/java/com/capgemini/courseApi/top/TopicController.java
@@ -4,6 +4,7 @@ package com.capgemini.courseApi.top;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -12,10 +13,13 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 	
-@RequestMapping("/top")
+    @RequestMapping("/top")
 	public List<Topic> getAllTopics() {
 		return  topicService.getAllTopics();
-				
-	
 	}
+
+    @RequestMapping("/top/{id}")
+    public Topic getTopic(@PathVariable String id) {
+    	return topicService.getTopic(id);
+    }
 }
