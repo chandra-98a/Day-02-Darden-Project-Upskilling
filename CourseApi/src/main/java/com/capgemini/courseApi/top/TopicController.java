bffd836429e241fa1175ffbe48b33c30aa9ba817
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-22 12:16:26 +0530

    "Topic Controller returns list of topics"

---
+++ CourseApi/src/main/java/com/capgemini/courseApi/top/TopicController.java
@@ -0,0 +1,21 @@
+package com.capgemini.courseApi.top;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TopicController {
+@RequestMapping("/top")
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring","spring framework","spring framework description"),
+				new Topic("java","core java","core java description"),
+				new Topic("javascript","javascript","javascript description")
+				
+				);
+				
+		
+	}
+}
