bc3236698c21de452aef1be33ce61f764e252a24
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-22 11:07:25 +0530

    "created TopicController with RequestMapping"

---
+++ DayTwoSpringBoot/src/main/java/com/capgemini/DayTwoSpringBoot/topic/TopicController.java
@@ -0,0 +1,13 @@
+package com.capgemini.DayTwoSpringBoot.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public String getAllTopics() {
+		return "All Topics";
+	}
+}
