8da3e872a835b50719275c850a7968eb4847dc06
chandra-98a <chandrakalaa2022@gmail.com>
2023-11-22 11:03:14 +0530

    "created spring boot project with spring initializer"

---
+++ DayTwoSpringBoot/src/main/java/com/capgemini/DayTwoSpringBoot/DayTwoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.DayTwoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayTwoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayTwoSpringBootApplication.class, args);
+	}
+
+}
---
+++ DayTwoSpringBoot/src/test/java/com/capgemini/DayTwoSpringBoot/DayTwoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.DayTwoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayTwoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
